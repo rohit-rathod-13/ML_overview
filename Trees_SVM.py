d49ba860713577a933be8ef6e53325f033281d11
Rohit Rathod <83444956+rohit-rathod-13@users.noreply.github.com>
2021-12-04 17:10:40 +0530

    Update Trees_SVM.py

--- Trees_SVM.py
+++ Trees_SVM.py
@@ -1,1 +1,26 @@
+#Decision Tree
+from sklearn import tree
 
+x =[[0,0],[1,1]]
+y =[0,1]
+
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(x,y)
+
+clf.predict([[-2,-1]])
+
+#array([0])
+
+#Support Vector Machine
+
+import numpy as np
+
+x=np.array([[-1,-1],[-2,-1],[1,1],[2,1]])
+y=np.array([1,1,2,2])
+
+from sklearn.svm import SVC
+
+clf=SVC()
+clf.fit(x,y)
+
+print(clf.predict([[8,1]]))
